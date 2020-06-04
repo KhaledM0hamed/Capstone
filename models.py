e39037280089976394ccde3b81ce6f23f609ece1
Khaled Mohamed <khaledmzakria1@gmail.com>
2020-06-04 10:21:06 +0200

    models updates and helpers added

--- models.py
+++ models.py
@@ -1,1 +1,68 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# app = Flask(__name__)
+
+db = SQLAlchemy()
+
+
+def setup_db(app):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:ASDasd225588@localhost:5432/fsnd'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    migrate = Migrate(app, db)
+
+
+class Actor(db.Model):
+    __tablename__ = "Actor"
+    actor_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String(), nullable=False)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'actor_id': self.actor_id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
+class Movie(db.Model):
+    __tablename__ = "Movie"
+    movie_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+
+    def format(self):
+        return {
+            'movie_id': self.movie_id,
+            'title': self.title,
+            'description': self.description
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
